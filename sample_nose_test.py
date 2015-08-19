e85b7f1f1a81434c58dbd556bc6488cabb6eaf41
Neil <neil@Neils-MacBook-Pro.local>
2015-08-18 17:04:59 -0700

    Adding changes before I send to Jack

--- sample_nose_test.py
+++ sample_nose_test.py
@@ -1,7 +1,6 @@
 import unittest
 import os
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 class PythonOrgSearch(unittest.TestCase):
     _multiprocess_can_split_ = True
@@ -23,19 +22,14 @@ class PythonOrgSearch(unittest.TestCase):
             command_executor = "http://%s:%s@ondemand.saucelabs.com:80/wd/hub" % (username, access_key),
             desired_capabilities = capabilities)
 
-    def test_search_in_python_org(self):
-        driver = self.driver
-        driver.get("http://www.python.org")
-        self.assertIn("Python", driver.title)
-        elem = driver.find_element_by_name("q")
-        elem.send_keys("pycon")
-        elem.send_keys(Keys.RETURN)
-        assert "No results found." not in driver.page_source
-
-    def test_search_in_python_org123(self):
-        driver = self.driver
-        driver.get("http://www.github.com")
-        self.assertIn("GitHub", driver.title)
-
     def tearDown(self):
         self.driver.quit()
+
+    def test_google(self):
+        driver = self.driver
+        driver.get("http://www.google.com")
+        if not "Google" in driver.title:
+            raise Exception("Unable to load google page!")
+        elem = driver.find_element_by_name("q")
+        elem.send_keys("Sauce Labs")
+        elem.submit()
